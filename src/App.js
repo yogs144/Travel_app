8a1d8a80315304a9fc963bcb9b5ae44e561f8957
widyatmoko <yogawpy@yahoo.com>
2021-02-14 13:48:10 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import 'assets/scss/style.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello world</p>
+    </div>
+  );
+}
+
+export default App;
