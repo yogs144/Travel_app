4f7299698547072080c9027ec33beb2a0ef7b537
Yogs144 <yoga123447@gmail.com>
2021-02-22 07:13:30 +0700

    menambahkan header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 
 import 'assets/scss/style.scss';
+import LandingPages from 'pages/LandingPages';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello world</p>
+      <Router>
+        <Route path ="/" component ={LandingPages}></Route>
+      </Router>
     </div>
   );
 }
---
+++ src/parts/IconText.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Button from 'elements/button'
+
+export default function IconText() {
+    return (
+        <Button className="brand-text-icon" href="" type="link">
+            Stay<span className="text-gray-900">cation</span>
+        </Button>
+    )
+}
